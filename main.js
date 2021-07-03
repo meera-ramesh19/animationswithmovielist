d86251e46298acd7a6a3a4f018314c75f36205f1
meera-ramesh19 <meeraramesh19@outlook.com>
2021-07-02 22:01:30 -0400

    fixed search function and corrected json data

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const imagePath = "https://image.tmdb.org/t/p/w1280"
-
 document.querySelector(".button-container")
     .addEventListener("click", () => {
-        let text = document.getElementById("filter-jobs").value;
+        let text = document.getElementById("filter-jobs").value.toLowerCase();
+        console.log(text);
         getJobs().then(jobs => {
             let filteredJobs = filterJobs(jobs, text);
-            showJobs(filteredJobs)
+            console.log("main", filteredJobs)
+            showJobs(filteredJobs);
         })
     })
 
@@ -13,22 +13,25 @@ function getJobs() {
     return fetch("data.json")
         .then(response => response.json())
         .then(data => {
+            console.log(data)
             return data
         })
 }
 
 function filterJobs(jobs, searchText) {
     if (searchText) {
+
+        console.log("in filter", jobs)
         let filteredJobs = jobs.filter(job => {
             if (job.title.toLowerCase().includes(searchText) ||
                 job.original_language.toLowerCase().includes(searchText) ||
-                job.popularity.toLowerCase().includes(searchText) ||
                 job.overview.toLowerCase().includes(searchText)) {
                 return true;
             } else {
                 return false;
             }
         })
+        console.log("infilter", filteredJobs)
         return filteredJobs;
     } else {
         return jobs;
@@ -37,31 +40,32 @@ function filterJobs(jobs, searchText) {
 
 
 
-
 function showJobs(jobs) {
     let jobsContainer = document.querySelector(".jobs-container");
     let jobsHTML = "";
-
-    let pics = jobs.results.poster_path;
-    let images = `https: //image.tmdb.org/t/p/original/${jobs.results.poster_path}`
-    console.log(images)
-    jobs = jobs.results;
+    const imagePath = "https://image.tmdb.org/t/p/w1280";;
+    // let pics = jobs.results.poster_path;
+    // let images = `https: //image.tmdb.org/t/p/original/${jobs.results.poster_path}`
+    // console.log(images)
+    // jobs = jobs.results;
     // <img src= "${job.poster_path}"/>
 
     jobs.forEach(job => {
+
         let { title, overview, release_date, poster_path, genre_ids, vote_average, adult, original_language } = job;
-        let pg13 = adult ? 'Yes' : 'No';
+        let pg13 = job.adult ? 'Yes' : 'No';
+        console.log(job)
         jobsHTML += `
             <div class="job-tile">
                 <div class="top">
-                    
+
              <img src='${imagePath + poster_path}' alt='${title} image' />
 
                     <span class="material-icons more_horiz">more_horiz</span>
                 </div>
                 <div class="rolename">
                     <span>${title}</span>
-                    
+
                 </div>
 
                 <div class="description">
@@ -81,9 +85,10 @@ function showJobs(jobs) {
                      <h3>PG-13:<span>${pg13}</span></h3>
                      </div>
             </div>
-           
+
         `
     })
+
     jobsContainer.innerHTML = jobsHTML;
 }
 
